4de128197fcd7f3fb142782471a4eba2de426fe8
Ondřej Kotas <ok@krtkovo.eu>
2024-09-11 23:53:03 +0200

    1.0

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTime.Location = new System.Drawing.Point(12, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(180, 37);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00:00.0";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(180, 32);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 87);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(180, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 119);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTime);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Stopwatch";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch; // Instance stopek
+        private Timer timer; // Timer pro aktualizaci GUI
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicializace stopek
+            stopwatch = new Stopwatch();
+
+            // Inicializace timeru
+            timer = new Timer();
+            timer.Interval = 100; // Aktualizace GUI každých 100 ms
+            timer.Tick += Timer_Tick;
+
+            // Nastavíme výchozí hodnotu labelu
+            lblTime.Text = "00:00:00.0";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (stopwatch.IsRunning)
+            {
+                // Zastavíme stopky
+                stopwatch.Stop();
+                timer.Stop();
+                btnStart.Text = "Start";
+            }
+            else
+            {
+                // Spustíme stopky
+                stopwatch.Start();
+                timer.Start();
+                btnStart.Text = "Stop";
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Zastavení a resetování stopek
+            stopwatch.Reset();
+            timer.Stop();
+            lblTime.Text = "00:00:00.0";
+            btnStart.Text = "Start";
+        }
+
+        // Aktualizace labelu s časem
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Formátování času do hh:mm:ss.0
+            TimeSpan ts = stopwatch.Elapsed;
+            lblTime.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:0}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 100);
+        }
+    }
+}
